67c14096f819388a5c2d10bbd6f6826524c3e4e3
aelieber1 <80122879+aelieber1@users.noreply.github.com>
2023-04-18 03:33:07 -0500

    updates to file

--- Week10_HW11_CLT.py
+++ Week10_HW11_CLT.py
@@ -3,6 +3,9 @@ Computational Physics PHSX 815 | Homework #11
 
 - Write a program that illustrates, in some way, the Central Limit Theorem (preferably with one or more figures)
 - For example, the Central Limit Theorem indicates that the normalized sum (i.e. the arithmetic mean) of random variables, each independently sampled from the same distribution, will asymptotically follow a Gaussian distribution. Try sampling a continuous variable from a probability distribution that is *not* a Gaussian - repeat this N times and take the average. Then repeat *that* again many times (so M "experiments" each with N samples) - what does this distribution of averages look like? As you increase N?
+
+        - As you increase N the distribution of averages becomes closer and closer to a
+          Gaussian distribution.
    
    
 Sources Consulted:
